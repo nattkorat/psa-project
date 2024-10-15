13c6374edea86ef057bcdb6eeff90ece5b29c8c1
natt <natt.korat@cadt.edu.kh>
2024-10-15 11:14:07 +0700

    [feature] seller.order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\Order;
+use App\Models\User;
+use App\Models\Product;
 
 class OrderController extends Controller
 {
@@ -13,8 +18,9 @@ class OrderController extends Controller
     }
 
     public function index(){
-
-        return view('seller.order');
+        $seller_id = Auth::user()->id;
+        $orders = Order::where('seller_id', $seller_id)->get();
+        return view('seller.order', compact('orders'));
 
     }
 }
--- resources/views/seller/order.blade.php
+++ resources/views/seller/order.blade.php
@@ -3,5 +3,32 @@
 @section('content')
     <h1 class="text-primary">Order List</h1>
     <hr>
+    @if(count($orders) <= 0)
+        <h3 class="text-danger text-center">No Product yet!</h3>
+    @else
+        <table class="table table-striped">
+            <thead>
+                <th>Product Profile</th>
+                <th>Product Name</th>
+                <th>Price</th>
+                <th>Description</th>
+            </thead>
+            <tbody>
+                @foreach($orders as $order)
+                    <tr>
+                        <td>
+                        <img src="{{ asset('storage/' . $product->image) }}" class="card-img-top" alt="{{ $product->name }}" style="width: 100px;">
+                        </td>
+                        <td>{{ $order->name }}</td>
+                        <td>{{ $order->amount }}</td>
+                        <td>{{ $order->created_at }}</td>
+                        <td>
+                            <a href="{{ route('order.details', $order->user_id) }}" class="btn btn-info">
+                                Details</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+    @endif
 
 @endsection
